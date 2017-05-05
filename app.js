6684917bc3d4033766abbb81e5f6a5468cc26400
Anna Zareba-Linka <anempi@gmail.com>
2017-05-05 23:50:17 +0200

    JS 50% done

--- app.js
+++ app.js
@@ -1,145 +1,87 @@
 $(document).ready(function() {
 
-  //Defaults player's turn is X, while computer's is O
-  var personTurn="X";
-  var computersTurn="O";
-  //Array stores values, that we will check later for a winner
-  var turns=["#","#","#","#","#","#","#","#","#"];
-  //Keeps track if it is the computer's turn (false when person's turn)
-  var gameOn = false;
-  //Keeps track of all spots, max = 9
-  var count = 0;
+  var addition = $('#plus5Clock');
+  var subtraction = $('#minus5Clock');
+  var minutesSession = $("#num");
+  var count = parseInt(minutesSession.html() );
 
-  //Reset the game after each win or square
-  function reset() {
-    turns = ["#","#","#","#","#","#","#","#","#"];
-    count = 0;
-    $('.tic').text("#").css("background-color", "#46484a");
-    setTimeout(function() {
-      $("h2").text("");
-    }, 1500);
-    gameOn = true;
-  };
+  var additionBreak = $('#add5Break');
+  var subtractionBreak = $('#minus5Break');
+  var minutesBreak = $("#breakNum");
+  var countBreak = parseInt(minutesBreak.html() );
 
-  //Change player's turn to O and computer's to X
-  $('#turnO').click(function() {
-    personTurn="O";
-    computersTurn = "X";
-    $('#turnX').removeClass('btn-active');
-    $('#turnO').addClass('btn-active');
-    reset();
+  var start = $('#start');
+
+  $('#reset').hide();
+
+//On click susbtracts 5 minutes from session time
+  subtraction.click(function (){
+    //can not have negative time, so time have  to be more than 0
+    if (count>5) {
+      count-=5;
+      minutesSession.html(count);
+    }
   });
 
-  //Change player's turn to X and computer's to O
-  $('#turnX').click(function() {
-    personTurn="X";
-    computersTurn="O";
-    $('#turnO').removeClass('btn-active');
-    $('#turnX').addClass('btn-active');
-    reset();
+//On click adds 5 minutes to session time
+  addition.click(function (){
+      count+=5;
+      minutesSession.html(count);
   });
 
-  //Player choosing spot
-  $('.tic').click(function() {
-    var slot = $(this).attr('id');
-    playerTurn (personTurn, slot);
+//On click susbtracts 5 minutes from break time
+  subtractionBreak.click(function (){
+    //can not have negative time, so time have to be more than 0
+    if (countBreak>5) {
+      countBreak-=5;
+      minutesBreak.html(countBreak);
+    }
   });
 
-  //Person plays
-  function playerTurn (personTurn, id) {
-    var spotTaken = $("#"+id).text();
-    if (spotTaken === "#") {
-      count++;
-      $("#" + id).text(personTurn);
-      $("#"+ id).css("background-color", "rgba(45, 86, 134, 0.77)");
-      var taken = true;
-      turns[id] = personTurn;
+//On click adds 5 minutes to break time
+  additionBreak.click(function (){
+      countBreak+=5;
+      minutesBreak.html(countBreak);
+  });
 
-      winCondition (turns, personTurn);
+//Counts down the session time, after click on start
+  start.click( function() {
+    //Hides buttons: start, + and - 5minutes, and break buttons and time
+    start.hide();
+    addition.hide();
+    subtraction.hide();
+    $(".breakDiv").hide();
 
-      if (gameOn===false) {
-        computerTurn();
-        winCondition(turns, computersTurn);
+    var counter = setInterval (timer, 1000);
+
+    function timer() {
+      count-=1;
+      if (count>=0) {
+        minutesSession.html(count);
+      } else {
+        clearInterval(counter);
       }
     }
-  };
+  });
 
-  //Computer plays
-  function computerTurn() {
-    //Used to brake while loop
-    var taken = false;
-    while(taken === false && count !== 5) {
-      //Generate computer's random turn
-      var computersMove = (Math.random()*10).toFixed();
-      var move = $("#"+ computersMove).text();
-      if (move === "#") {
-        $("#"+ computersMove).text(computersTurn);
-        $("#"+ computersMove).css("background-color", "rgba(45, 86, 134, 0.77)");
-        taken=true;
-        turns[computersMove] = computersTurn;
-      }
-    }
-  }
-
-  function winCondition (turnArray, currentTurn) {
-    var infoText = $('h2');
-    // Option 1: All the same in top row
-    if (turnArray[0]===currentTurn && turnArray[1] === currentTurn && turnArray[2] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 2: All the same in middle row
-    else if (turnArray[3]===currentTurn && turnArray[4] === currentTurn && turnArray[5] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 3: All the same in bottom row
-    else if (turnArray[6]===currentTurn && turnArray[7] === currentTurn && turnArray[8] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 4: All the same in left column
-    else if (turnArray[0]===currentTurn && turnArray[3] === currentTurn && turnArray[6] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 5: All the same in middle column
-    else if (turnArray[1]===currentTurn && turnArray[4] === currentTurn && turnArray[7] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 6: All the same in right column
-    else if (turnArray[2]===currentTurn && turnArray[5] === currentTurn && turnArray[8] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 7: All the same diagonal left-top to right-bottom
-    else if (turnArray[0]===currentTurn && turnArray[4] === currentTurn && turnArray[8] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 8: All the same diagonal right-top to left-bottom
-    else if (turnArray[2]===currentTurn && turnArray[4] === currentTurn && turnArray[6] === currentTurn) {
-      gameOn = true;
-      reset();
-      infoText.text("Player " +currentTurn + " wins");
-    }
-    // Option 9: Tie
-    else if (!(turnArray.includes("#"))) {
-      gameOn = true;
-      reset();
-      infoText.text("It's a tie");
-    } else {
-      gameOn = false;
-    }
-  }
+// //Counts down the break time, when session time is over
+//   start.click( function() {
+//     //Hides buttons: start , + and - 5minutes
+//     start.hide();
+//     additionBreak.hide();
+//     subtractionBreak.hide();
+//
+//     var counter = setInterval (timer, 1000);
+//
+//     function timer() {
+//       countBreak-=1;
+//       if (countBreak>=0) {
+//         minutesBreak.html(count);
+//       } else {
+//         clearInterval(counter);
+//       }
+//     }
+//   });
 
 
 });
