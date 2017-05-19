80f69b2502114426856cf2d47583b1be92ec1ea4
Anna Zareba-Linka <anempi@gmail.com>
2017-05-19 23:53:06 +0200

    Add logic - clock counting down

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ $(document).ready(function() {
   var addition = $('#plus5Clock');
   var subtraction = $('#minus5Clock');
   var minutesSession = $("#num");
-  var count = parseInt(minutesSession.html() );
+  var sessionTitle = $('#sesionTitle');
+  var count = parseInt(minutesSession.html());
 
   var additionBreak = $('#add5Break');
   var subtractionBreak = $('#minus5Break');
   var minutesBreak = $("#breakNum");
+  var breakTitle = $('#breakTitle');
   var countBreak = parseInt(minutesBreak.html() );
 
-  var start = $('#start');
 
-  $('#reset').hide();
+
+  var start = $('#start');
+  var reset = $('#reset')
 
 //On click susbtracts 5 minutes from session time
   subtraction.click(function (){
@@ -46,13 +49,18 @@ $(document).ready(function() {
 
 //Counts down the session time, after click on start
   start.click( function() {
-    //Hides buttons: start, + and - 5minutes, and break buttons and time
+    //Hides unecessery buttons
+    reset.hide();
     start.hide();
     addition.hide();
     subtraction.hide();
-    $(".breakDiv").hide();
+    additionBreak.hide();
+    subtractionBreak.hide();
+    minutesBreak.hide();
+    breakTitle.hide();
 
     var counter = setInterval (timer, 1000);
+    count*=60;
 
     function timer() {
       count-=1;
@@ -60,28 +68,51 @@ $(document).ready(function() {
         minutesSession.html(count);
       } else {
         clearInterval(counter);
+        var startBreak = setInterval(breakTimer, 1000);
       }
+
+      //changing counting into minutes and seconds
+      if (count%60 >= 10){
+        minutesSession.html(Math.floor(count/60) + ":" + count%60)
+      }
+
+      // Counts down the break time, when session gets to 0
+      function breakTimer(){
+
+        if (countBreak>0) {
+          sessionTitle.hide();
+          breakTitle.show();
+          minutesBreak.show();
+          countBreak-=1;
+          minutesBreak.html(countBreak);
+        } else {
+          clearInterval(startBreak);
+          reset.show();
+        }
+
+        if (countBreak%60 >= 10){
+          minutesBreak.html(Math.floor(count/60) + ":" + count%60)
+        }
+      }
+
     }
+
   });
 
-// //Counts down the break time, when session time is over
-//   start.click( function() {
-//     //Hides buttons: start , + and - 5minutes
-//     start.hide();
-//     additionBreak.hide();
-//     subtractionBreak.hide();
-//
-//     var counter = setInterval (timer, 1000);
-//
-//     function timer() {
-//       countBreak-=1;
-//       if (countBreak>=0) {
-//         minutesBreak.html(count);
-//       } else {
-//         clearInterval(counter);
-//       }
-//     }
-//   });
+  reset.click(function(){
+    start.show();
+    addition.show();
+    subtraction.show();
+    additionBreak.show();
+    subtractionBreak.show();
+    minutesBreak.show();
+    breakTitle.show();
+    count = 0;
+    countBreak = 0;
+    reset.hide();
+  });
+
+
 
 
 });
